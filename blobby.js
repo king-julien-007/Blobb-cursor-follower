bee03f448ae44d39eb278701dacda5defc2ced80
Ajmal Muhammed J N <96969560+king-julien-007@users.noreply.github.com>
2024-08-26 20:58:58 +0530

    Add files via upload

---
+++ blobby.js
@@ -0,0 +1,14 @@
+/* -- Glow effect -- */
+
+const blob = document.getElementById("blob");
+
+document.body.onpointermove = event => { 
+  const { clientX, clientY } = event;
+  
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 3000, fill: "forwards" });
+}
+
+
